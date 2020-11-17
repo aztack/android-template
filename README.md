663f9e142b7df8284e59916b800acad3f7db3ac7
wangweihua <wangweihua@vipkid.com.cn>
2020-11-17 14:23:18 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+An Android Kotlin Project Template
+====
---
+++ meta.js
@@ -0,0 +1,48 @@
+const $fs = require('fs');
+const $path = require('path');
+
+module.exports = {
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "label": "Project name, no space or punctuations, for example: MyProject"
+    },
+    "package": {
+      "type": "string",
+      "required": true,
+      "label": "android package, for example: com.company.project"
+    },
+    "description": {
+      "type": "string",
+      "required": true,
+      "label": "Project description",
+      "default": "A Android Kotlin Project"
+    },
+    "author": {
+      "type": "string",
+      "label": "Author"
+    }
+  },
+  complete: function (data, opts) {
+    const cwd = $path.join(process.cwd(), data.inPlace ? '' : data.destDirName);
+    const name = data.name;
+
+    // create folders
+    ['libs', `src/main/java/${data.package.replace(/\./g, '/')}`].forEach(function(dir){
+      console.log(`creating ${dir} folder`);
+      $fs.mkdirSync($path.resolve(cwd, `app/${dir}`), {recursive: true});
+    });
+  },
+  "skipInterpolation": [
+    "app/libs",
+    "app/src/res/drawable",
+    "app/src/res/drawable-v24",
+    "app/src/res/mipmap-anydpi-v26",
+    "app/src/res/mipmap-hdpi",
+    "app/src/res/mipmap-mdpi",
+    "app/src/res/mipmap-xhdpi",
+    "app/src/res/mipmap-xxhdpi",
+    "app/src/res/mipmap-xxxhdpi"
+  ]
+}
